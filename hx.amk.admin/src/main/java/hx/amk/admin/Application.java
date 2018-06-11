e017ee07e51e9ea5443d7630d0d1d125ff49c3f9
aschenxiaoran <as_chenxiaoran@163.com>
2018-06-11 17:06:14 +0800

    增加springcloud测试模块

--- hx.amk.admin/src/main/java/hx/amk/admin/Application.java
+++ hx.amk.admin/src/main/java/hx/amk/admin/Application.java
@@ -8,20 +8,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+
 @SpringBootApplication
 @MapperScan("hx.amk.admin.mapper")
 @EnableTransactionManagement
 public class Application {
 
-//    @Autowired
-//    DataSource dataSource;
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
-//
-//    public void run(String...args) throws Exception{
-//        System.out.println("data source:"+dataSource);
-//    }
+
 }
